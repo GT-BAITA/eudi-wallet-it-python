21164463bef41739d87f83866feda7a8a6566b46
alisonnRB <alisonrbjs@gmail.com>
2025-11-13 18:37:29 -0300

    first adjust

---
+++ oidFed/satosa/backends/endpoints/pre_request_endpoint.py
@@ -0,0 +1,70 @@
+from typing import Callable
+from uuid import uuid4
+
+from satosa.attribute_mapping import AttributeMapper
+from satosa.context import Context
+from satosa.internal import InternalData
+from satosa.response import Response, Redirect
+
+from pyeudiw.satosa.utils.html_template import Jinja2TemplateHandler
+from pyeudiw.trust.dynamic import CombinedTrustEvaluator
+
+
+class PreRequestHandler():
+
+    def __init__(
+            self, 
+            config: dict, 
+            internal_attributes: dict[str, dict[str, str | list[str]]], 
+            base_url: str, 
+            name: str,
+            auth_callback_func: Callable[[Context, InternalData], Response],
+            converter: AttributeMapper,
+            trust_evaluator: CombinedTrustEvaluator
+        ) -> None:
+        """
+        Initialize the AuthorizationHandler with the given configuration, internal attributes, base URL, and name.
+
+        :param config: Configuration dictionary for the handler.
+        :param internal_attributes: Internal attributes mapping.
+        :param base_url: Base URL for the handler.
+        :param name: Name of the handler.
+
+        :raises ValueError: If storage or QR code settings are not configured.
+        """
+
+        super().__init__(config, internal_attributes, base_url, name, auth_callback_func, converter)
+        pass
+    
+    def endpoint(self, context: Context) -> Response:
+        """
+        This endpoint is called by the User-Agent/Wallet Instance before calling the request endpoint.
+        It initializes the session and returns the request_uri to be used by the User-Agent/Wallet Instance.
+
+        :type context: the context of current request
+        :param context: the request context
+        :type internal_request: satosa.internal.InternalData
+        :param internal_request: Information about the authorization request
+
+        :return: a response containing the request_uri
+        :rtype: satosa.response.Response
+        """
+        pass
+    
+    @staticmethod
+    def _same_device_http_response(response_url: str) -> Response:
+        return Redirect(response_url)
+
+    def _cross_device_http_response(self, response_url: str, state: str) -> Response:
+        result = self.template.qrcode_page.render(
+            {
+                "qrcode_color": self.qrcode_settings["color"],
+                "qrcode_text": response_url,
+                "qrcode_size": self.qrcode_settings["size"],
+                "qrcode_logo_path": self.qrcode_settings["logo_path"],
+                "qrcode_expiration_time": self.qrcode_settings["expiration_time"],
+                "state": state,
+                "status_endpoint": self.absolute_status_url,
+            }
+        )
+        return Response(result, content="text/html; charset=utf8", status="200")
